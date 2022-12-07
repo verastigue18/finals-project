f759b5cf74a105e702d2d15106fa65d26396cca2
Jhon Brian Arce <111772143+verastigue18@users.noreply.github.com>
2022-12-07 08:57:15 +0800

    Update finalProj.c

--- finalProj.c
+++ finalProj.c
@@ -21,49 +21,53 @@ void registList(void);
 void listRegistered(void);
 
 //This is for Simple Calculator
-void simpleCal(void);
-void basicComp(void);
+void simpleCal(void); //menu
+void basicComp(void); //case 1
+void areaOFShape(void); //case2
+void areaTri(void);
 
+void greet(void);
 
+//global Variable
+char select;
+int opt;
 
 int main()
 {
+    system("Color A");
     banner();
     return 0;
 }
 
 void banner(void)
 {
-    char select;
+    printf("\n\n\t\t---------------------------------- Welcome to ----------------------------------  \n");
+    printf("\t\t               #########   ######    ######  ##        ##  ######                 \n");
+    printf("\t\t               ##      ## ##    ##       ##  ####    #### ##    ##                \n");
+    printf("\t\t               ##      ## ##    ##       ##  ## ##  ## ## ##    ##                \n");
+    printf("\t\t               #########  ########       ##  ##   ##   ## ########                \n");
+    printf("\t\t               ##      ## ##    ##       ##  ##        ## ##    ##                \n");
+    printf("\t\t               ##      ## ##    ## ##    ##  ##        ## ##    ##                \n");
+    printf("\t\t               #########  ##    ##  ######   ##        ## ##    ##                \n");
+    printf("\t\t               +--------------------------------------------------+               \n");
+    printf("\t\t               | Brian      Aron    Joshua     Maxwell     Albert |               \n");
+    printf("\t\t               +--------------------------------------------------+               \n");
+    printf("\t\t--------------------------------------------------------------------------------\n\n");
 
-    system("Color A");
-    printf("---------------------------------- Welcome to ----------------------------------  \n");
-    printf("               #########   ######    ######  ##        ##  ######                 \n");
-    printf("               ##      ## ##    ##       ##  ####    #### ##    ##                \n");
-    printf("               ##      ## ##    ##       ##  ## ##  ## ## ##    ##                \n");
-    printf("               #########  ########       ##  ##   ##   ## ########                \n");
-    printf("               ##      ## ##    ##       ##  ##        ## ##    ##                \n");
-    printf("               ##      ## ##    ## ##    ##  ##        ## ##    ##                \n");
-    printf("               #########  ##    ##  ######   ##        ## ##    ##                \n");
-    printf("               +--------------------------------------------------+               \n");
-    printf("               | Brian      Aron    Joshua     Maxwell     Albert |               \n");
-    printf("               +--------------------------------------------------+               \n");
-    printf("--------------------------------------------------------------------------------  \n");
-
-    printf("Do You Want to Use this Program? [Y/N] : ");
-    scanf("%c", &select);
+    printf("\t\tDo You Want to Use this Program? press [Y/N] : ");
+    select = getch();
+    //scanf("%c", &select);
 
     switch(select)
     {
         case 'Y': case 'y' :
+            system("cls");
             mainMenu();
-            //printf("Menu");
             break;
+
         case 'N': case 'n':
             system("cls");
-            printf("exit");
-            //exit();
-            exit(0);
+            greet();
             break;
 
         default:
@@ -74,30 +78,26 @@ void banner(void)
 
 void mainMenu(void)
 {
-    system("cls");
+    printf("\n\n\t\t                                     MENU                                      \n");
+    printf("\t\t  ================================================================================ \n");
+    printf("\t\t          ___________________________       ___________________________            \n");
+    printf("\t\t         /___________________________  |   /___________________________  |         \n");
+    printf("\t\t        |                            | |  |                            | |         \n");
+    printf("\t\t        | [1] Registration Form      | |  | [6] Color Selector         | |         \n");
+    printf("\t\t        | [2] Simple Calculator      | |  | [7] Grade Range            | |         \n");
+    printf("\t\t        | [3] Integers Identifier    | |  | [8] Intergers Comparison   | |         \n");
+    printf("\t\t        | [4] Temperature Converter  | |  | [9] Back                   | |         \n");
+    printf("\t\t        | [5] Qualification to Vote  | |  |                            | |         \n");
+    printf("\t\t        |____________________________|/   |____________________________|/        \n\n");
+    printf("\t\t  ================================================================================ \n\n");
 
-    printf("\n                                     MENU                                      \n");
-    printf("================================================================================ \n");
-    printf("        ___________________________       ___________________________            \n");
-    printf("       /___________________________  |   /___________________________  |         \n");
-    printf("      |                            | |  |                            | |         \n");
-    printf("      | [1] Registration Form      | |  | [6] Color Selector         | |         \n");
-    printf("      | [2] Simple Calculator      | |  | [7] Grade Range            | |         \n");
-    printf("      | [3] Simple Calculator      | |  | [8] Intergers Comparison   | |         \n");
-    printf("      | [4] Temperature Converter  | |  | [9] Integers Identifier    | |         \n");
-    printf("      | [5] Qualification to Vote  | |  | [0] Bitwise                | |         \n");
-    printf("      |____________________________|/   |____________________________|/        \n\n");
-    printf("================================================================================ \n");
-
-    int opt;
-    printf("Pick a number that you Want to Use [1-9] : ");
+    printf("\t\t  Pick a number that you Want to Use [1-9] : ");
     scanf("%i", &opt);
 
     switch(opt)
     {
         case 1:
             system("cls");
-            printf("Registration Form\n");
             registList();
             break;
 
@@ -109,42 +109,42 @@ void mainMenu(void)
 
         case 3:
             system("cls");
+            printf("UNDER PROCESS! \n");
             break;
 
         case 4:
             system("cls");
-            printf("Temperature Converter \n");
+            printf("UNDER PROCESS! \n");
             //tempConvert();
             break;
 
         case 5:
             system("cls");
-            printf("Qualitification to Vote \n");
+            printf("UNDER PROCESS! \n");
             //qualiVote();
             break;
 
         case 6:
             system("cls");
-            printf("Color Selection \n");
+            printf("UNDER PROCESS! \n");
             //colorSelec();
             break;
 
         case 7:
             system("cls");
-            printf("Grade Range \n");
+            printf("UNDER PROCESS! \n");
             //gradeRange();
             break;
 
         case 8:
             system("cls");
-            printf("Intergers \n");
+            printf("UNDER PROCESS! \n");
             //integer();
             break;
 
         case 9:
             system("cls");
-            printf("Back");
-            //banner();
+            banner();
             break;
 
         default:
@@ -157,15 +157,14 @@ void mainMenu(void)
 //This is for Registration or case 1
 void registList(void)
 {
-    system("cls");
-    int opt;
-    printf("\t\t ==========Registration List==========\n\n");
+
+    printf("\n\n\t\t ==========Registration List==========\n\n");
 
     printf("\t\t [1] - Registration Form                \n");
     printf("\t\t [2] - List of Registered               \n");
     printf("\t\t [3] - Back                           \n\n");
 
-    printf("Pick a Number : ");
+    printf("\t\t Pick a Number : ");
     scanf("%i", &opt);
 
     switch(opt)
@@ -183,6 +182,7 @@ void registList(void)
         case 3:
             system("cls");
             mainMenu();
+            break;
 
     }
 
@@ -241,37 +241,60 @@ void regisForm(void)
     printf("What is your Third Choice? ");
     scanf(" %50[^\n]", &thirdChoice);
 
-    printf("=========================================  \n");
-    printf("           REGISTRATION FORM               \n");
-    printf("=========================================\n\n");
+    system("cls");
 
-    printf("NAME           : %s\n", name);
-    printf("AGE            : %i\n", age);
-    printf("BIRTHDAY       : %s\n", birthday);
-    printf("ADDRESS        : %s\n", add);
-    printf("CONTACT NO.    : %s\n", contactNum);
-    printf("EMAIL ADD.     : %s\n", emailAdd);
-    printf("CITIZENSHIP    : %s\n", citizen);
-    printf("FATHER'S NAME  : %s\n", fatherName);
-    printf("MOTHER'S NAME  : %s\n", motherName);
+    printf("\n\n\t\t =========================================  \n");
+    printf("\t\t            REGISTRATION FORM               \n");
+    printf("\t\t =========================================\n\n");
 
-    printf("========================================   \n");
-    printf("          EDUCATIONAL ATTAINTMENT          \n");
-    printf("=========================================\n\n");
+    printf("\t\t NAME           : %s\n", name);
+    printf("\t\t AGE            : %i\n", age);
+    printf("\t\t BIRTHDAY       : %s\n", birthday);
+    printf("\t\t ADDRESS        : %s\n", add);
+    printf("\t\t CONTACT NO.    : %s\n", contactNum);
+    printf("\t\t EMAIL ADD.     : %s\n", emailAdd);
+    printf("\t\t CITIZENSHIP    : %s\n", citizen);
+    printf("\t\t FATHER'S NAME  : %s\n", fatherName);
+    printf("\t\t MOTHER'S NAME  : %s\n", motherName);
 
-    printf("PRIMARY        : %s\n", educPri);
-    printf("SECONDARY      : %s\n", educSec);
-    printf("TERTIARY       : %s\n\n", educTer);
+    printf("\t\t ========================================   \n");
+    printf("\t\t           EDUCATIONAL ATTAINTMENT          \n");
+    printf("\t\t =========================================\n\n");
 
-    printf("1ST CHOICE     : %s\n", firstChoice);
-    printf("2ND CHOICE     : %s\n", secondChoice);
-    printf("3RD CHOICE     : %s\n", thirdChoice);
+    printf("\t\t PRIMARY        : %s\n", educPri);
+    printf("\t\t SECONDARY      : %s\n", educSec);
+    printf("\t\t TERTIARY       : %s\n\n", educTer);
+
+    printf("\t\t 1ST CHOICE     : %s\n", firstChoice);
+    printf("\t\t 2ND CHOICE     : %s\n", secondChoice);
+    printf("\t\t 3RD CHOICE     : %s\n\n", thirdChoice);
+
+    printf("\t\t BACK press [0]           EXIT press [1] : ");
+    select = getch();
+
+    switch(select)
+    {
+        case '0':
+            system("cls");
+            registList();
+            break;
+
+        case '1':
+            system("cls");
+            greet();
+            break;
+
+        default:
+            system("cls");
+            printf("Invalid");
+            break;
+    }
 }
 
 void listRegistered(void)
 {
     //Prelim Act1
-    printf("\t\t            List of Registered People          \n\n");
+    printf("\n\n\t\t            List of Registered People        \n\n");
 
     printf("\t\t ==============================================  \n");
     printf("\t\t               REGISTRATION FORM                 \n");
@@ -384,12 +407,32 @@ void listRegistered(void)
     printf("\t\t 3RD CHOICE     : BSIT                         \n\n");
 
     printf("\t\t ----------------------------------------------\n\n");
+
+    printf("\t\t BACK press [0]               EXIT press [1] : ");
+    select = getch();
+
+    switch(select)
+    {
+        case '0':
+            system("cls");
+            registList();
+            break;
+
+        case '1':
+            system("cls");
+            greet();
+            break;
+
+        default:
+            system("cls");
+            printf("Invalid");
+            break;
+    }
 }
 
 //This is for Simple Calculator of or case 2
 void simpleCal(void)
 {
-    int opt;
 
     printf("\t\t           Simple Calculator          \n\n");
 
@@ -397,8 +440,9 @@ void simpleCal(void)
     printf("\t\t  [2] - Area of Shape                   \n");
     printf("\t\t  [3] - Grade Calculation               \n");
     printf("\t\t  [4] - Population Growth Rate          \n");
-    printf("\t\t  [5] - Back                            \n");
-    printf("\t\t  ______________________________________\n");
+    printf("\t\t  [5] - Bitwise                         \n");
+    printf("\t\t  [6] - Back                            \n");
+    printf("\t\t______________________________________\n\n");
     printf("\t\t  Pick a Number : ");
     scanf("%i", &opt);
 
@@ -411,7 +455,7 @@ void simpleCal(void)
 
         case 2:
             system("cls");
-            //areaOFShape();
+            areaOFShape();
             break;
 
         case 3:
@@ -426,6 +470,11 @@ void simpleCal(void)
 
         case 5:
             system("cls");
+            //bitwise();
+            break;
+
+        case 6:
+            system("cls");
             mainMenu();
             break;
     }
@@ -435,11 +484,9 @@ void basicComp(void)
 {
     //Prelim Examination.    This code is from MAXELL JANE CALPO
 
-    char select;
-
     float add, sub, product, quote, num1, num2, num3, expo;
 
-    printf("\nFirst number : ");
+    printf("First number : ");
     scanf("%f", &num1);
 
     printf("Second number : ");
@@ -484,4 +531,111 @@ void basicComp(void)
 
 
     }
+}
+
+void areaOFShape(void)
+{
+    printf("\n\n\t\t ==========Area of Shape==========\n\n");
+
+    printf("\t\t [1] - Area of Triangle                 \n");
+    printf("\t\t [2] - Area of Square                   \n");
+    printf("\t\t [3] - Area of Circle                   \n");
+    printf("\t\t [4] - Area of Parallelogram            \n");
+    printf("\t\t [5] - Back                           \n\n");
+
+    printf("\t\t Pick a Number : ");
+    scanf("%i", &opt);
+
+    switch(opt)
+    {
+        case 1:
+            system("cls");
+            areaTri();
+            break;
+
+        case 2:
+            system("cls");
+            printf("Under Process!\n");
+            break;
+
+        case 3:
+            system("cls");
+            printf("Under Process!\n");
+            break;
+
+        case 4:
+            system("cls");
+            printf("Under Process!\n");
+            break;
+
+        case 5:
+            system("cls");
+            mainMenu();
+            break;
+
+
+    }
+}
+
+void areaTri(void)
+{
+    //Prelim Exercise Area of Triangle.    This line of Code came from ARON JAMES BETINOL
+
+    float b, h, area;
+
+    printf("\t\t \" Area of Triangle\" \n\n");
+
+    // For Base
+    printf("\t\t Base = ");
+    scanf("%f", &b);
+
+    //For Height
+    printf("\t\t Height = ");
+    scanf("%f", &h);
+
+    system("cls");
+
+    area = (b*h)/2;
+
+    // Answer/Equal/Total
+    printf("\n\n\t\t Total Area = %.2f\n\n", area);
+
+     printf("\t\t BACK press [0]               EXIT press [1] : ");
+    select = getch();
+
+    switch(select)
+    {
+        case '0':
+            system("cls");
+            areaOFShape();
+            break;
+
+        case '1':
+            system("cls");
+            greet();
+            break;
+
+        default:
+            system("cls");
+            printf("Invalid");
+            break;
+    }
+
+}
+
+void greet(void)
+{
+    printf("\n\n\t\t================================================================================ \n");
+    printf("\t\t        _________                               _    _                               \n");
+    printf("\t\t        ___   ___ _   _  _____  _   _  _   _   | |  | | _____  _   _                 \n");
+    printf("\t\t           | |   | | | ||     || | | || | | |  | |  | ||  _  || | | |                \n");
+    printf("\t\t           | |   | |_| || |_| ||  \\| || |/ /   |  \\/  || | | || | | |              \n");
+    printf("\t\t           | |   |     ||  _  || |\\  ||   |     \\    / | | | || | | |              \n");
+    printf("\t\t           | |   | | | || | | || | | || |\\ \\     |  |  | | | || | | |              \n");
+    printf("\t\t           | |   | | | || | | || | | || | | |    |  |  | |_| || |_| |                \n");
+    printf("\t\t           |_|   |_| |_||_| |_||_| |_||_| |_|    |__|  |_____||_____|                \n");
+    printf("\t\t        +-----------------------------------------------------------+                \n");
+    printf("\t\t        |                    For Using Our Program                  |                \n");
+    printf("\t\t        +-----------------------------------------------------------+                \n");
+    printf("\t\t================================================================================     \n");
 }
